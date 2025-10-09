fe48ed288ebb2f2a84010619887d1ccaf254fa35
Luidi1 <146306312+Luidi1@users.noreply.github.com>
2025-10-09 12:11:31 -0300

    Delete Formacao_APIsComNodeJsEExpress/3374-nodejs-express-sequelize directory

--- Formacao_APIsComNodeJsEExpress/3374-nodejs-express-sequelize/src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = require('./routes'); //Não precisa passar o index.js, pois é um padrão do JS que pega automático.
-
-const app = express();
-routes(app); //esse app, vai ser o app do index.js
-
-
-module.exports = app;
